dae11df5f02a2be26a4d112ab8ed22e00a82b1b3
Monica Olinescu <monica.olinescu@gmail.com>
2015-03-10 11:03:09 -0700

    Display and number buttons

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,18 +8,28 @@
 
 import UIKit
 
+// TODO: Rename this to CalculatorViewController
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    // properies (instance variables)
+    // IBOutlet is not part of swift. It's an annotation added by XCode.
+    // declare a variable called display of type UILabel when dragging and dropping
+    @IBOutlet weak var display: UILabel!     // ! says display is an Optional, but unwrap it automatically. Called 'Implicitly unwrapped optional'
+    // IBOutlet weak var display: UILabel?  // display is an Optional UILabel
+    
+    var userIsTyping: Bool = false  // instance variable set to false
+    
+    // type touchUpInside
+    // IBAction func appendDigit(sender: UIButton) -> Double {  //example return
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!  // String? type. Optional String. This var is an Optional and if it is set, it is a String.
+                                         // the ! unwraps the optional. i.e. returns the string
+        println("digit = \(digit)")
+        if(display.text! == "0") {
+          display.text = ""
+        }
+        display.text = display.text! + digit
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
